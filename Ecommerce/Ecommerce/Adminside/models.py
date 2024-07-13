5a45c8db07ea5af88e29a574496abfe84d63c5bb
Mohit Dadwal <mohitkdadwal2001@gmail.com>
2024-07-13 13:06:42 +0530

    CRUD
    Add, Show, Delete, Update the fields.

---
+++ Ecommerce/Ecommerce/Adminside/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.category_name
+
+class Product(models.Model):
+    category_id = models.ForeignKey(Category,on_delete=models.CASCADE )
+    product_name = models.CharField(max_length=255)
+    product_description = models.CharField(max_length=255 ,default="")
+    product_price = models.CharField(max_length=255, default="")
+    product_image = models.ImageField(upload_to="products", default="")
+
+    def __str__(self):
+        return self.product_name
