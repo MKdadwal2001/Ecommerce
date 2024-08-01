962941d07e7e6fe7bf3981db44c9987aa4a1a374
Mohit Dadwal <mohitkdadwal2001@gmail.com>
2024-08-01 12:50:57 +0530

    HTML pages, Model
    create HTML (user_login,user_register,) create customuser model,

--- Ecommerce/Adminside/models.py
+++ Ecommerce/Adminside/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -15,5 +16,18 @@ class Product(models.Model):
     product_price = models.CharField(max_length=255, default="")
     product_image = models.ImageField(upload_to="products", default="")
     is_teends_or_just_arived = models.BooleanField( default=True ) 
+
     def __str__(self):
         return self.product_name
+
+class CustomUser(models.Model):
+    phone_number = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    user_image = models.ImageField(upload_to="USer")
+    pincode =  models.CharField(max_length=255)
+    locality = models.CharField(max_length=255)
+    landmark = models.CharField(max_length=255)
+    user_id = models.OneToOneField(User,on_delete=models.PROTECT,default="") 
+    
+    def __str__(self):
+        return self.user_id.username
--- Ecommerce/Userside/views.py
+++ Ecommerce/Userside/views.py
@@ -1,21 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from Adminside.models import *
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def user_register(request):
-    categories_data = Category.objects.all()[:6]
-    trendy_products = Product.objects.filter(is_teends_or_just_arived=True)[:8]
-    just_arrive_products = Product.objects.filter(is_teends_or_just_arived=False)[:8]
-    return render(request,"Userside/user_register.html",{"categories_data":categories_data,"trendy_products":trendy_products,"just_arrive_products":just_arrive_products})
+    if request.method == "POST":
+        username = request.POST.get('username', None)
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        address = request.POST.get('address')
+        phonenumber = request.POST.get('phonenumber')
+        user_image = request.POST.get('userimage')
+        Pincode  = request.POST.get('pincode')
+        locality = request.POST.get('locality')
+        landmark = request.POST.get('landmark') 
+
+        n_user = User.objects.create_user(username=username, email=email, password=password)
+        n_user.first_name = firstname
+        n_user.last_name = lastname
+        n_user.save()
+        print(User.objects.get(id=n_user.id))
+        
+        
+        c_user = CustomUser()
+        c_user.phone_number = phonenumber
+        c_user.address = address
+        c_user.user_image = user_image
+        c_user.pincode = Pincode
+        c_user.locality = locality
+        c_user.landmark = landmark
+        c_user.user_id = User.objects.get(id=n_user.id)
+        c_user.save()
+        return HttpResponse(c_user)
+    else:
+        return render(request,"Userside/user_register.html")
 
 def user_login(request):
-    categories_data = Category.objects.all()[:6]
-    trendy_products = Product.objects.filter(is_teends_or_just_arived=True)[:8]
-    just_arrive_products = Product.objects.filter(is_teends_or_just_arived=False)[:8]
-    return render(request,"Userside/user_login.html",{"categories_data":categories_data,"trendy_products":trendy_products,"just_arrive_products":just_arrive_products})
+    if request.method == "POST":
+        userName = request.POST["username"]
+        passWord = request.POST["password"]
 
+        categories_data = Category.objects.all()[:6]
+        trendy_products = Product.objects.filter(is_teends_or_just_arived=True)[:8]
+        just_arrive_products = Product.objects.filter(is_teends_or_just_arived=False)[:8]
+
+    else:
+        return render(request,"Userside/user_login.html")
+    
 def index(request):
     categories_data = Category.objects.all()[:6]
     trendy_products = Product.objects.filter(is_teends_or_just_arived=True)[:8]
@@ -27,4 +62,4 @@ def user_dashboard(request):
     trendy_products = Product.objects.filter(is_teends_or_just_arived=True)[:8]
     just_arrive_products = Product.objects.filter(is_teends_or_just_arived=False)[:8]
     return render(request,"Userside/dashboard.html",{"categories_data":categories_data,"trendy_products":trendy_products,"just_arrive_products":just_arrive_products})
-    
+
