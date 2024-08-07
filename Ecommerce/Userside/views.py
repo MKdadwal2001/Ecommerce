ecba50c14448af7584ca4bc9fa37b45da3aced91
Mohit Dadwal <mohitkdadwal2001@gmail.com>
2024-08-07 12:42:14 +0530

    Resolve Error
    1. Resolve error and Update the code for the user registeration on userside panel.
    2. Delete the whole database and created new, so that custom user model will work fine

--- Ecommerce/Userside/views.py
+++ Ecommerce/Userside/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from Adminside.models import *
 from django.contrib.auth.models import User
@@ -8,34 +8,38 @@ from django.contrib.auth.models import User
 def user_register(request):
     if request.method == "POST":
         username = request.POST.get('username', None)
-        password = request.POST.get('password')
+        password = request.POST.get('passwd')
         email = request.POST.get('email')
         firstname = request.POST.get('firstname')
         lastname = request.POST.get('lastname')
         address = request.POST.get('address')
         phonenumber = request.POST.get('phonenumber')
         user_image = request.POST.get('userimage')
-        Pincode  = request.POST.get('pincode')
+        pincode  = request.POST.get('pincode')
         locality = request.POST.get('locality')
         landmark = request.POST.get('landmark') 
 
+        print("User details:- ", username, password, email, firstname, lastname)
+        print("Other details:- ", phonenumber, user_image,pincode,locality,landmark)
+
+        
         n_user = User.objects.create_user(username=username, email=email, password=password)
         n_user.first_name = firstname
         n_user.last_name = lastname
         n_user.save()
-        print(User.objects.get(id=n_user.id))
+        print("Created user id:- ", User.objects.get(id=n_user.id))
         
         
         c_user = CustomUser()
         c_user.phone_number = phonenumber
         c_user.address = address
         c_user.user_image = user_image
-        c_user.pincode = Pincode
+        c_user.pincode = pincode
         c_user.locality = locality
         c_user.landmark = landmark
         c_user.user_id = User.objects.get(id=n_user.id)
         c_user.save()
-        return HttpResponse(c_user)
+        return redirect(user_login)
     else:
         return render(request,"Userside/user_register.html")
 
