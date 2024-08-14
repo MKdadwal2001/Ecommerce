60ba6bb7c1eb93c536330eb5fbc4ee5b8aaf9782
Mohit Dadwal <mohitkdadwal2001@gmail.com>
2024-08-14 10:17:49 +0530

    Products and Categories
    Show all the Products and Categories on the home page....

--- Ecommerce/Userside/urls.py
+++ Ecommerce/Userside/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path("user-dashboard",views.user_dashboard,name="user-dashboard"),
     path("user-login",views.user_login,name="user-login"),
     path("user-register",views.user_register,name="user-register"),
+    path("user-logout",views.user_logout, name="user-logout"),
+
+    path("category-based-products/<int:pk>",views.category_based_products,name="category-based-products"),
+    path("all-products",views.view_all_products,name="all-products"),
+    path("all-categories",views.all_categories,name="all-categories"),
+
 ]
--- Ecommerce/Userside/views.py
+++ Ecommerce/Userside/views.py
@@ -6,11 +6,13 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 
 
+
+
 # Create your views here.
 def user_register(request):
     if request.method == "POST":
         username = request.POST.get('username', None)
-        password = request.POST.get('password')
+        password = request.POST.get('passwd')
         email = request.POST.get('email')
         firstname = request.POST.get('firstname')
         lastname = request.POST.get('lastname')
@@ -61,6 +63,7 @@ def user_login(request):
     else:
         return render(request,"Userside/user_login.html")
 
+
 def index(request):
     categories_data = Category.objects.all()[:6]
     trendy_products = Product.objects.filter(is_teends_or_just_arived=True)[:8]
@@ -71,5 +74,29 @@ def user_dashboard(request):
     categories_data = Category.objects.all()[:6]
     trendy_products = Product.objects.filter(is_teends_or_just_arived=True)[:8]
     just_arrive_products = Product.objects.filter(is_teends_or_just_arived=False)[:8]
-    return render(request,"Userside/dashboard.html",{"categories_data":categories_data,"trendy_products":trendy_products,"just_arrive_products":just_arrive_products})
+    return render(request,"Userside/dashboard.html",{"categories_data":categories_data,"trendy_products":trendy_products,"just_arrive_products":just_arrive_products})  
+
+def user_logout(request):
+    logout(request)
+    messages.success(request,"Logout Successfully.")
+    return redirect(user_dashboard)
+
+def category_based_products(request,pk):
+    categories_data = Category.objects.all()[:6]
+    filtered_products_according_category = Product.objects.filter(category_id = Category.objects.get(id=pk))
+    print("Filtered products according to the category :- ", filtered_products_according_category)
+    return render(request,"Userside/category_based_products.html",{"categories_data":categories_data,"filtered_products_according_category":filtered_products_according_category})
+
+def view_all_products(request):
+    categories_data = Category.objects.all()[:6]
+    all_products = Product.objects.all()
+    return render(request,"Userside/view_all_products.html",{
+        "categories_data":categories_data,
+        "all_products":all_products
+    })
+
+def all_categories(request):
+    categories_data = Category.objects.all()[:6]
+    all_categories = Category.objects.all()
+    return render(request,"Userside/all_categories.html",{"categories_data":categories_data,"all_categories":all_categories})
 
